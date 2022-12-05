fc2150f49e5c5b475ea4b020a9e38c3803e79716
JackDouma <97615212+JackDouma@users.noreply.github.com>
2022-12-05 12:31:45 -0500

    validation

--- CardDealer/Main.cpp
+++ CardDealer/Main.cpp
@@ -14,10 +14,34 @@ int main() {
 	// random seed
 	srand(time(nullptr));
 	
-	//ask how many cards the user would like
+	bool invalid = true;
 	int numCards;
-	cout << "How many cards would you like to draw? ";
-	cin >> numCards;
+	
+	// loops until a valiud input is given
+	while (invalid)
+	{
+		//ask how many cards the user would like
+		cout << "How many cards would you like to draw? ";
+		cin >> numCards;
+
+		// if user inputs any non-numeric characters, then make them try again
+		if (cin.fail()) 
+		{
+			cout << "Please enter a numerical value.\n";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+		}
+		// if user inputs a number less than 1 or over 52, then make them try again
+		if (numCards < 1 || numCards > 52)
+		{
+			cout << numCards << " is not a valid entry. Please ensure your number is between 1 and 52\n";
+		}
+		else
+		{
+			invalid = false;
+		}
+	}
 	cout << endl;
 	
 	// calculate remaining cards
